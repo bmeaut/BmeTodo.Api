4180cd02a987f7530c382f09c59f6536606eaf6e
Tóth Tibor <toth.tibor@aut.bme.hu>
2019-10-05 16:53:56 +0200

    validációk

--- BmeTodo.Api/Models/TodoItem.cs
+++ BmeTodo.Api/Models/TodoItem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +10,23 @@ namespace BmeTodo.Api.Models
     public class TodoItem
     {
         public int Id { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public bool IsDone { get; set; }
+
+        [Required(ErrorMessage = "Cím megadása kötelező")]
+        [MaxLength(50, ErrorMessage = "Túl hosszú cím")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Leírás megadása kötelező")]
+        [MaxLength(1500, ErrorMessage = "Túl hosszú leírás")]
         public string Description { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public DateTimeOffset Deadline { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        [Range((int)Priority.High, (int)Priority.Low, ErrorMessage = "Nem megfelelő prioritás érték")]
         public Priority Priority { get; set; }
     }
 }
