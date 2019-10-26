070918bc451b47c6cdae8ff5396d882965fa4e6e
Tóth Tibor <toth.tibor@aut.bme.hu>
2019-10-26 22:22:55 +0200

    aspnetcore 3.0

--- src/BmeTodo.Api/Startup.cs
+++ src/BmeTodo.Api/Startup.cs
@@ -11,13 +11,10 @@ using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace BmeTodo.Api
 {
@@ -30,23 +27,24 @@ namespace BmeTodo.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
+
             services.AddSingleton<TodoService>();
+
             services.AddSwaggerGen(o =>
             {
-                o.SwaggerDoc("v1", new Info { Title = "BME Todo Service", Version = "v1" });
-                o.SchemaFilter<EnumFilter>();
+                o.SwaggerDoc("v1", new OpenApiInfo { Title = "BME Todo Service", Version = "v1" });
+                o.SchemaFilter<AutoRestSchemaFilter>();
                 o.DescribeAllEnumsAsStrings();
                 o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
             });
+
             services.AddProblemDetails(o => o.Map<EntityNotFoundException>(ex => new StatusCodeProblemDetails(StatusCodes.Status404NotFound)));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -59,15 +57,19 @@ namespace BmeTodo.Api
             }
 
             app.UseProblemDetails();
+            app.UseRouting();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BME Todo Service");
                 c.RoutePrefix = string.Empty;
             });
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
---
+++ src/BmeTodo.Api/Swagger/AutoRestSchemaFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BmeTodo.Api.Swagger
+{
+    public class AutoRestSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            var type = context.ApiModel.Type;
+            if (type.IsEnum)
+            {
+                schema.Extensions.Add(
+                    "x-ms-enum",
+                    new OpenApiObject
+                    {
+                        ["name"] = new OpenApiString(type.Name),
+                        ["modelAsString"] = new OpenApiBoolean(true)
+                    }
+                );
+            };
+        }
+    }
+}
